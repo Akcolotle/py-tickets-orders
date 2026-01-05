db462bf71e3f7b0117e63f3c1757033f3d0948d8
Artem <artemp2208@gmail.com>
2026-01-05 13:19:42 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Count, F, IntegerField, ExpressionWrapper
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order,
+)
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,6 +21,8 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    OrderReadSerializer,
+    OrderCreateSerializer,
 )
 
 
@@ -56,3 +67,50 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+    def get_queryset(self):
+        queryset = MovieSession.objects.select_related(
+            "movie",
+            "cinema_hall"
+        )
+
+        date = self.request.query_params.get("date")
+        movie = self.request.query_params.get("movie")
+
+        if date:
+            queryset = queryset.filter(show_time__date=date)
+
+        if movie:
+            queryset = queryset.filter(movie_id=movie)
+
+        if self.action == "list":
+            queryset = queryset.annotate(
+                tickets_sold=Count("tickets"),
+                tickets_available=ExpressionWrapper(
+                    F("cinema_hall__capacity") - F("tickets_sold"),
+                    output_field=IntegerField(),
+                )
+            )
+
+        return queryset
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return (
+            Order.objects.filter(user=self.request.user)
+            .prefetch_related(
+                "tickets__movie_session__movie",
+                "tickets__movie_session__cinema_hall",
+            )
+        )
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderReadSerializer
+        if self.action == "create":
+            return OrderCreateSerializer
+        return OrderReadSerializer
